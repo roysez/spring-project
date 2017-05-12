d863628c250952824825e7a0f8256612abd29355
Sergiy Balukh <serhii.balukh.pz.2016@lpnu.ua>
2017-05-12 16:26:05 +0300

    js for user_profile.js

---
+++ src/main/webapp/static/js/user_profile.js
@@ -0,0 +1,100 @@
+/**
+ * Created by roysez on 05.05.2017.
+ */
+
+
+    function deleteUser(username) {
+        $.ajax({
+            type: "DELETE",
+            url: '/users/'+username,
+
+            "statusCode": {
+                401: function () {
+                    $('.main').html('User with username: ' + username + ', wasn\'t  deleted');
+                    $('.main').addClass('alert alert-danger');
+                },
+                404: function () {
+                    $('.main').html('User not found');
+                    $('.main').addClass('alert alert-danger');
+                },
+                200: function () {
+                    $('.main').html('User with username: ' + username + ', was successful deleted');
+                    $('.main').addClass('alert alert-success');
+                }
+            }
+        });
+
+    }
+
+
+    function editUser() {
+        $('.edit-off').css('display','none');
+        $('.edit-on').css('display','block');
+        $('button.edit-on').css('display','inline-block');
+    }
+
+    function cancelEdit() {
+        $('.edit-off').css('display','inline-block');
+        $('.edit-on').css('display','none');
+
+    }
+
+
+    function submitEditUser(username){
+        var newUsername = $('#username').val();
+        var newFirstName = $('#firstName').val();
+        var newLastName = $('#lastName').val();
+        var newEmail = $('#email').val();
+
+        var user = {
+            'ssoId' : newUsername,
+            'firstName' : newFirstName,
+            'lastName' : newLastName,
+            'email' : newEmail
+        };
+
+        function setNewFields() {
+            $('#origin-username').html(newUsername);
+            $('#origin-firstName').html(newFirstName);
+            $('#origin-lastName').html(newLastName);
+            $('#origin-email').html(newEmail);
+        }
+
+        $.ajax({
+            type: "PUT",
+            url: '/users/'+username,
+            data: JSON.stringify(user),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            dataType: 'json',
+            "statusCode": {
+                401: function () {
+                    cancelEdit();
+                    $('.alert-block').html('User with username: ' + username + ', can\'t be edited');
+                    $('.alert-block').addClass('alert alert-danger');
+                },
+                404: function () {
+                    cancelEdit();
+                    $('.alert-block').html('User not found');
+                    $('.alert-block').addClass('alert alert-danger');
+                },
+                200: function () {
+                    //cancelEdit();
+
+                    window.location.replace("/users/"+newUsername);
+                    setTimeout(function(){
+                        //do what you need here
+                        $('.alert-block').html('Profile has been successfully edited');
+                        $('.alert-block').addClass('alert alert-success');
+                    }, 5000);
+
+                    // setNewFields();
+                }
+            }
+
+        });
+
+    }
+
